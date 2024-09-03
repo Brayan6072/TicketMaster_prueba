5e792dd95181fe6e1fd4c2c2b62441d8b3a764bf
Brayan6072 <brayan.delgado6072@alumnos.udg.mx>
2024-09-03 16:56:36 -0600

    Validaciones

--- js/signup.js
+++ js/signup.js
@@ -3,20 +3,71 @@ signupForm.addEventListener('submit', (e)=>{
     e.preventDefault()
     const name = document.querySelector('#name').value
     const email = document.querySelector('#email').value
-    const password = document.querySelector('#password').value
+    const password = document.querySelector('#password').value  
 
-    const Users = JSON.parse(localStorage.getItem('users')) || []
-    const isUserRegistered = Users.find(user => user.email === email)
-    if(isUserRegistered){
-        return alert('El usuario ya esta registado!')
+    
+    
+    
+    if(validateInput()){
+        const Users = JSON.parse(localStorage.getItem('users')) || []
+        const isUserRegistered = Users.find(user => user.email === email && user.username == name)
+        if(isUserRegistered){
+            return alert('El usuario ya esta registado!')
+        }
+        Users.push({username: name, email: email, password: password})
+        localStorage.setItem('users', JSON.stringify(Users))
+        alert('Registro Exitoso!')
+        window.location.href = 'login.html'
     }
-
-    Users.push({name: name, email: email, password: password})
-    localStorage.setItem('users', JSON.stringify(Users))
-    alert('Registro Exitoso!')
-    window.location.href = 'login.html'
+    
 
 })
-function convertToLowercase(input) {
-    input.value = input.value.toLowerCase();
+
+function validateInput() {
+    let validacion = 0;
+    var password = document.getElementById('password').value;
+    var nombre =  document.getElementById('name').value;
+    const especial = /[!@#\$%\^\&*\)\(+=._-]/;
+    
+
+    if(nombre.length < 4 ){
+        alert('No se aceptan menos de 4 caracteres en el usuario'); validacion++; }
+
+    
+
+    if(password.length != 8){
+        alert('La contraseña debe ser de 8 carateres');
+        validacion++;
+    }
+
+    for(let i = 0; i < nombre.length; i++){
+        if (nombre[i] === " ") {
+            alert("No se aceptan espacios");
+            validacion++  
+        }
+    }
+
+
+    if (nombre[0] === nombre[0].toLowerCase() && isNaN(nombre[0])) {
+        alert("El usuario no puede comenzar con una letra minúscula.");   
+        validacion++; 
+    }
+   
+    
+    if (especial.test(nombre[0])) {
+        alert("El usuario no puede iniciar con un carácter especial");
+        validacion++;
+    }
+
+    for(let i = 1 ; i < password.length -1; i++ ){
+        if(especial.test(password[i])){
+            alert('No se permiten caracteres especiales en medio de la contraseña');
+            validacion++;
+            
+        }  
+              
+    }
+    
+    return validacion === 0;
+    
 }
